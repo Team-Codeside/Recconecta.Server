851a53fde7c8948370795aab6fbfd124cb3427a4
Andrepinheiroo <andrepinheiro.code@gmail.com>
2023-11-25 22:33:42 -0300

    add token verification

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,9 +1,11 @@
 const router = require('express').Router()
 
 const UserController = require('../controllers/UserController')
+const User = require('../models/User')
 
 //rotas do postman
 router.post('/register', UserController.register)
 router.post('/login', UserController.login)
+router.get('/checkuser', UserController.checkUser)
 
 module.exports = router;
